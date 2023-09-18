644393ab89591779d8a2601217a63018815434f0
Ayarachi Fuentes Hector Daniel <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-09-18 17:54:33 -0300

    .

--- index.js
+++ index.js
@@ -21,4 +21,21 @@ container.innerHTML = cards;
 
 // index.js
 
+const pokedex = document.getElementById('pokedex');
+
+function search_pokemon() {
+  let input = document.getElementById('searchbar').value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('card');
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = 'none';
+    } else {
+      x[i].style.display = 'list-item';
+    }
+  }
+}
+
+document.getElementById('searchbar').addEventListener('keyup', search_pokemon);
 
