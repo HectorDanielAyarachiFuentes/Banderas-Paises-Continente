4e98e3b8b97e1ac51c20b76d68b1c91c294529a3
Ayarachi Fuentes Hector Daniel <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-09-18 19:25:07 -0300

    .

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 const container = document.querySelector('.container');
 let card = '';
 let cards = '';
-for (let e of array) {
-    let name = e.name.common;
-    let flag = e.flags.png?? e.flags.svg ;
-    let region=e.region;
 
-   card = `<div class="card">
-   <div class="cardH">
-       <h3> ${name}</h2>
-   </div>
-   <div class="cardB">
-       <img src="${flag}" alt="${name}">
-   </div>
-   <div class="cardF"> ${region}</div>
-</div>`
+for (let e of array) {
+    let name = e.name.spa || e.name.common;
+    let flag = e.flags.png || e.flags.svg;
+    let region = e.region;
+    let capital = e.capital || "No disponible";
+    let borders = e.borders || [];
+
+    // Formatea el área con la unidad de medida "km²" si está disponible
+    let area = e.area ? `${e.area.toLocaleString()} km²` : "No disponible";
+
+    // Formatea la población con puntos como separadores de miles
+    let population = e.population ? e.population.toLocaleString() : "No disponible";
+
+    card = `<div class="card">
+        <div class="cardH">
+            <h3>${name}</h3>
+        </div>
+        <div class="cardB">
+            <img src="${flag}" alt="${name}">
+        </div>
+        <div class="cardF">Región: ${region}</div>
+        <div class="cardF">Capital: ${capital}</div>
+        <div class="cardF">Área: ${area}</div>
+        <div class="cardF">Población: ${population}</div>
+        <div class="cardF">Países Vecinos: ${borders.join(', ')}</div>
+    </div>`;
+
     cards += card;
 }
+
 container.innerHTML = cards;
 
+
+container.innerHTML = cards;
+
+
+
 // index.js
+
 
 const pokedex = document.getElementById('pokedex');
 
@@ -38,4 +59,3 @@ function search_pokemon() {
 }
 
 document.getElementById('searchbar').addEventListener('keyup', search_pokemon);
-
